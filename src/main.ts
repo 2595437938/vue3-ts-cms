33bb5914869cdc5bef07e4737ffb55443c171a4f
sunxiantao <2595437938@qq.com>
2022-06-12 23:48:59 +0800

    feat(master): 引用element-plus,封装router,封装store

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,12 @@
 import { createApp } from "vue"
 import App from "./App.vue"
-
-createApp(App).mount("#app")
-
-console.log("Hello World")
+import router from "./router"
+import store from "./store"
+import ElementPlus from "element-plus"
+import "element-plus/dist/index.css"
+import "./service/axios_demo"
+const app = createApp(App)
+app.use(router)
+app.use(store)
+app.use(ElementPlus)
+app.mount("#app")
