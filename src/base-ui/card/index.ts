b87e253ff9e3f366ac3dd95f35bd842bb8cc7f3c
sunxiantao <2595437938@qq.com>
2022-06-28 21:05:30 +0800

    feat: mian

--- src/base-ui/card/index.ts
+++ src/base-ui/card/index.ts
@@ -1,3 +1,3 @@
-import HyCard from "./src/card.vue"
+import XtCard from "./src/card.vue"
 
-export default HyCard
+export default XtCard
--- src/views/main/analysis/dashboard/dashboard.vue
+++ src/views/main/analysis/dashboard/dashboard.vue
@@ -2,32 +2,32 @@
   <div class="dashboard">
     <el-row :gutter="10">
       <el-col :span="7">
-        <hy-card title="分类商品数量(饼图)">
+        <xt-card title="分类商品数量(饼图)">
           <pie-echart :pieData="categoryGoodsCount"></pie-echart>
-        </hy-card>
+        </xt-card>
       </el-col>
       <el-col :span="10">
-        <hy-card title="不同城市商品销量">
+        <xt-card title="不同城市商品销量">
           <map-echart :mapData="addressGoodsSale"></map-echart>
-        </hy-card>
+        </xt-card>
       </el-col>
       <el-col :span="7">
-        <hy-card title="分类商品数量(玫瑰图)">
+        <xt-card title="分类商品数量(玫瑰图)">
           <rose-echart :roseData="categoryGoodsCount"></rose-echart>
-        </hy-card>
+        </xt-card>
       </el-col>
     </el-row>
 
     <el-row :gutter="10" class="content-row">
       <el-col :span="12">
-        <hy-card title="分类商品的销量">
+        <xt-card title="分类商品的销量">
           <line-echart v-bind="categoryGoodsSale"></line-echart>
-        </hy-card>
+        </xt-card>
       </el-col>
       <el-col :span="12">
-        <hy-card title="分类商品的收藏">
+        <xt-card title="分类商品的收藏">
           <bar-echart v-bind="categoryGoodsFavor"></bar-echart>
-        </hy-card>
+        </xt-card>
       </el-col>
     </el-row>
   </div>
@@ -37,7 +37,7 @@
 import { defineComponent, computed } from "vue"
 import { useStore } from "vuex"
 
-import HyCard from "@/base-ui/card"
+import XtCard from "@/base-ui/card"
 import {
   PieEchart,
   RoseEchart,
@@ -49,7 +49,7 @@ import {
 export default defineComponent({
   name: "dashboard",
   components: {
-    HyCard,
+    XtCard,
     PieEchart,
     RoseEchart,
     LineEchart,
